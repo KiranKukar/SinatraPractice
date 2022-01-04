489090650cfe952cfedc70b01aa1915ca359a3fb
KiranKukar <kiranrkukar@gmail.com>
2022-01-04 16:46:04 +0000

    added and image route to server

--- app.rb
+++ app.rb
@@ -12,3 +12,13 @@ end
 get '/plants' do
   'hallo Vera!'
 end
+
+get '/anothersecret' do
+  'the plant is called Vera'
+end
+
+get '/cat' do
+  "<div style='border: dashed cyan'>
+  <img src='https://i.imgur.com/jFaSxym.png' border: dashed red;>
+  </div>"
+end
