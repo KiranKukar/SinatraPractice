993782b4839cfc8d6917da66bcf42b5114b80a80
KiranKukar <kiranrkukar@gmail.com>
2022-01-04 17:07:42 +0000

    set up erb file in views and used ERB tags to add text with random name

--- app.rb
+++ app.rb
@@ -18,7 +18,5 @@ get '/anothersecret' do
 end
 
 get '/cat' do
-  "<div style='border: dashed cyan'>
-  <img src='https://i.imgur.com/jFaSxym.png' border: dashed red;>
-  </div>"
+  erb(:index)
 end
